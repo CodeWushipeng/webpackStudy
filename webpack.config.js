9dcf7035cc1a8f5a28525d3fb66c96eb0fe56c3f
wushipeng <18234097765@163.com>
2021-02-19 10:24:38 +0800

    完成css文件在开发态和生产态的提取使用

--- webpack.config.js
+++ webpack.config.js
@@ -4,112 +4,123 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 // 生产环境提取css
 var MiniCssExtractPlugin = require("mini-css-extract-plugin");
-module.exports = {
-  context: path.resolve(__dirname), //当前项目目录绝对路径
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"), //__dirname 当前模块的目录名
-    filename: "[name].bundle.js",
-    //publicPath: "/assets/", //输出解析文件的目录，url 相对于 HTML 页面也就是根目录
-    // library: {
-    //   //开发插件时需要该配置
-    //   type: "umd", //umd 通用模块定义规范，适合所有环境
-    //   name: "myFirstLibrary", //导出插件的名称
-    //   auxiliaryComment: {
-    //     //每种类型都插入不同的注释
-    //     root: "Root Comment",
-    //     commonjs: "CommonJS Comment",
-    //     commonjs2: "CommonJS2 Comment",
-    //     amd: "AMD Comment",
-    //   },
-    // },
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 9000,
-    overlay: {
-      //用于显示错误和警告
-      warnings: true,
-      errors: true,
+module.exports = (env) => {
+  console.log("NODE_ENV: ", env.NODE_ENV);
+  if (env.NODE_ENV === "development") {
+    process.env.NODE_ENV = "development";
+    console.log(process.env.NODE_ENV);
+  }
+  return {
+    context: path.resolve(__dirname), //当前项目目录绝对路径
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"), //__dirname 当前模块的目录名
+      filename: "[name].bundle.js",
+      //publicPath: "/assets/", //输出解析文件的目录，url 相对于 HTML 页面也就是根目录
+      // library: {
+      //   //开发插件时需要该配置
+      //   type: "umd", //umd 通用模块定义规范，适合所有环境
+      //   name: "myFirstLibrary", //导出插件的名称
+      //   auxiliaryComment: {
+      //     //每种类型都插入不同的注释
+      //     root: "Root Comment",
+      //     commonjs: "CommonJS Comment",
+      //     commonjs2: "CommonJS2 Comment",
+      //     amd: "AMD Comment",
+      //   },
+      // },
     },
-  },
-  resolve: {
-    //用于解析文件并赋予别名
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      api: path.resolve(__dirname, "src/api/"),
-    },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "My App",
-      template: path.resolve(__dirname, "src/index.html"),
-      favicon: path.resolve(__dirname, "src/assets/favicon.ico"),
-    }),
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "style.css",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          process.env.NODE_ENV !== "production"
-            ? "vue-style-loader"
-            : MiniCssExtractPlugin.loader,
-          "style-loader",
-          "css-loader",
-          "postcss-loader",
-        ],
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      compress: true,
+      port: 9000,
+      overlay: {
+        //用于显示错误和警告
+        warnings: true,
+        errors: true,
       },
-      {
-        test: /\.scss$/i,
-        use: [
-          {
-            loader: "sass-loader",
-            options: {
-              //处理缩进的scss语法
-              indentedSyntax: true,
-              // sass-loader version >= 8
-              sassOptions: {
-                indentedSyntax: true,
+    },
+    resolve: {
+      //用于解析文件并赋予别名
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+        api: path.resolve(__dirname, "src/api/"),
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "My App",
+        template: path.resolve(__dirname, "src/index.html"),
+        favicon: path.resolve(__dirname, "src/assets/favicon.ico"),
+      }),
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "style.css",
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            env.NODE_ENV !== "production"
+              ? "vue-style-loader"
+              : MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                esModule: false,  //此处设置为false是因为vue-style-loader4.1.2版本只接受esModule: false，而css-loader4.2之后的版本默认为true，会导致css样式不加载
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        options: {
-          plugins: [
-            [
-              "import-globals",
-              {
-                process: require.resolve("process/browser"),
-              },
-            ],
+            "postcss-loader",
           ],
         },
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        type: "asset",
-        // 小于 8kb 的文件，将会视为 inline 模块类型，否则会被视为 resource 模块类型。
-        // 自定义设置上限
-        parser: {
-          dataUrlCondition: {
-            maxSize: 8 * 1024,
+        {
+          test: /\.scss$/i,
+          use: [
+            {
+              loader: "sass-loader",
+              options: {
+                //处理缩进的scss语法
+                indentedSyntax: true,
+                // sass-loader version >= 8
+                sassOptions: {
+                  indentedSyntax: true,
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              [
+                "import-globals",
+                {
+                  process: require.resolve("process/browser"),
+                },
+              ],
+            ],
           },
         },
-      },
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-      },
-    ],
-  },
+        {
+          test: /\.(png|jpg|gif)$/,
+          type: "asset",
+          // 小于 8kb 的文件，将会视为 inline 模块类型，否则会被视为 resource 模块类型。
+          // 自定义设置上限
+          parser: {
+            dataUrlCondition: {
+              maxSize: 8 * 1024,
+            },
+          },
+        },
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+      ],
+    },
+  };
 };
